22ed9c6f8efb87b499612eebb0d050c6b9e39376
Alice-63 <alicetindag84@gmail.com>
2021-07-05 17:19:17 +0200

    created
    index.html
    style.css
    script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let val;
+
+val=5;
+
+
+console.log(val)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // if(a==2)
+    // {
+    //     for(i=0 ;i < allA.length ; i++)
+    // {
+    //     allA[i].style.display="none";
+    //     a=0;
+    // }
+
+    // }

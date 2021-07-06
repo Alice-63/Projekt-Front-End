3eb32bc3f993f16149babdc7b81a58e88b5a906b
Alice-63 <alicetindag84@gmail.com>
2021-07-06 20:03:30 +0200

    add

--- script.js
+++ script.js
@@ -1,54 +1,28 @@
-var bild1=document.getElementById("bild1");
-var bild2=document.getElementById("bild2");
-var bild3=document.getElementById("bild3");
+var bild=document.getElementById("bild");
 var h1=document.getElementById("h1");
-var h2=document.getElementById("h2")
-var h3=document.getElementById("h3")
-
-var slide=1;
-
+var number=1;
 
 function next()
 {
-   slide++;
-   
-   if(slide==2)
-   {
-       bild1.style.opacity="0";
-       bild2.style.opacity="100";
-   }
-   else if(slide==3)
-   {
-    bild2.style.opacity="0";
-    bild3.style.opacity="100";
-   }
-   else
-   {
-    bild3.style.opacity="0";
-    bild1.style.opacity="100";
-    slide=1;
-   }
-}
+    number++;
 
-function prev()
-{
- slide--;
- if(slide==0)
- {
-    bild1.style.opacity="0";
-    bild3.style.opacity="100";
-    slide=3;
- }
+    if(number==1)
+    {
+        bild.style.backgroundImage="url(img/"+number+".png)";
+        h1.innerHTML="Herzlichen Wilkommen"
+    }
+    else if(number==2)
+    {
+        bild.style.backgroundImage="url(img/"+number+".png)";
+        h1.innerHTML="Wir machen Musik!"
 
- else if(slide==1)
- {
-    bild2.style.opacity="0";
-    bild1.style.opacity="100";
- }
- else if(slide==2)
- {
-    bild3.style.opacity="0";
-    bild2.style.opacity="100";
+    }
+    else if(number==3)
+    {
+        bild.style.backgroundImage="url(img/"+number+".png)";
+        h1.innerHTML="Musik ist das Leben!"
+        number=1;
+    }
    
- }
+
 }

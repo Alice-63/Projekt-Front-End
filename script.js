1afc1e0d1dcaeda9115bafd1092fa2d823e9b6ea
Alice-63 <alicetindag84@gmail.com>
2021-07-06 16:41:47 +0200

    add

--- script.js
+++ script.js
@@ -1,33 +1,54 @@
+var bild1=document.getElementById("bild1");
+var bild2=document.getElementById("bild2");
+var bild3=document.getElementById("bild3");
+var h1=document.getElementById("h1");
+var h2=document.getElementById("h2")
+var h3=document.getElementById("h3")
 
-let val;
-
-val=5;
-
-
-console.log(val)
-
-
-
+var slide=1;
 
 
+function next()
+{
+   slide++;
+   
+   if(slide==2)
+   {
+       bild1.style.opacity="0";
+       bild2.style.opacity="100";
+   }
+   else if(slide==3)
+   {
+    bild2.style.opacity="0";
+    bild3.style.opacity="100";
+   }
+   else
+   {
+    bild3.style.opacity="0";
+    bild1.style.opacity="100";
+    slide=1;
+   }
+}
 
+function prev()
+{
+ slide--;
+ if(slide==0)
+ {
+    bild1.style.opacity="0";
+    bild3.style.opacity="100";
+    slide=3;
+ }
 
-
-
-
-
-
-
-
-
-
-
-    // if(a==2)
-    // {
-    //     for(i=0 ;i < allA.length ; i++)
-    // {
-    //     allA[i].style.display="none";
-    //     a=0;
-    // }
-
-    // }
+ else if(slide==1)
+ {
+    bild2.style.opacity="0";
+    bild1.style.opacity="100";
+ }
+ else if(slide==2)
+ {
+    bild3.style.opacity="0";
+    bild2.style.opacity="100";
+   
+ }
+}

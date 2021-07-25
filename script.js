3382dc0903777852de1231a50f6e75d44c9c0111
Alice-63 <alicetindag84@gmail.com>
2021-07-25 13:05:09 +0200

    add

--- script.js
+++ script.js
@@ -26,3 +26,26 @@ function next()
    
 
 }
+
+// navbar
+
+var allA=document.getElementsByTagName("a");
+var nav=document.getElementById("nav");
+var a=false;
+
+function show()
+{
+    
+    if(a)
+    {
+        nav.style.height="54px"
+
+    }
+    else
+    {
+        nav.style.height="340px"
+    }
+   
+    
+    a=!a;
+}
